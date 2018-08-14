47d67abd4b101132e56f939ced04d14cea21cf1a
lyz <lyz@meitu.com>
2018-08-14 15:12:41 +0800

    add cool-annotation  cool-processor

--- app/src/main/java/me/lyz/aptdemo/MainActivity.java
+++ app/src/main/java/me/lyz/aptdemo/MainActivity.java
@@ -1,10 +1,13 @@
 package me.lyz.aptdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.lyz.annotation.CoolAnnotation;
 
 public class MainActivity extends AppCompatActivity {
 
+    @CoolAnnotation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
